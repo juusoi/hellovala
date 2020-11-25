4324161c5c7ca131aefa6c6134bcb846149987b1
Juuso Issakainen <juuso.issakainen@gmail.com>
2020-11-25 15:46:39 +0200

    first version

---
+++ app.js
@@ -0,0 +1,15 @@
+function sayHello() {
+    alert('Greetings, traveller!');
+}
+function myName() {
+    alert('I am Juuso, nice to meet you');
+}
+function myThings() {
+    alert('I am a test lead')
+}
+function myCustomer() {
+    alert('I work at SOK')
+}
+function myExpertise() {
+    alert('My expertise is not JS :D')
+}
